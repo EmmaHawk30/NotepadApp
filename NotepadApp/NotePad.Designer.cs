d1ed3b783e176d08c1a65b26d32c2b1f1372d0ac
EmmaHawk30 <56272717+EmmaHawk30@users.noreply.github.com>
2022-02-17 14:46:42 +0000

    Add project files.

---
+++ NotepadApp/NotePad.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace NotepadApp
+{
+    partial class NotePad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(270, 594);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(188, 57);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(12, 23);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(729, 554);
+            this.rtbText.TabIndex = 2;
+            this.rtbText.Text = "";
+            // 
+            // NotePad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 700);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnSave);
+            this.Name = "NotePad";
+            this.Text = "NotePad";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox rtbText;
+    }
+}
---
+++ NotepadApp/NotePad.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotepadApp
+{
+    public partial class NotePad : Form
+    {
+        public NotePad()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {           
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.FileName = "TextFile"; //sets default file name
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = saveFileDialog1.FileName;
+                
+                if(!File.Exists(path))
+                {
+                    using(StreamWriter sw = File.CreateText(path))
+                    {
+                        for(int i = 0; i < rtbText.Lines.Length; i++)
+                        {
+                            sw.WriteLine(rtbText.Lines[i]);
+                        }
+                    }
+                }
+                else if (File.Exists(path))
+                {
+                    using(StreamWriter sw = File.CreateText(path))
+                    {
+                        for(int i = 0; i < rtbText.Lines.Length; i++)
+                        {
+                            sw.WriteLine(rtbText.Lines[i]);
+                        }
+                    }
+                }
+            }
+
+            //display NewFile form - asks user if they want to open a new notepad file
+            NewFile nf = new NewFile();
+            nf.Show();
+            this.Hide();
+
+            
+        }
+    }
+}
